fdbd3e9c5b3bdae005b019683c8bb13c4f1bdd72
Hanson Tram <htram.dev@gmail.com>
2023-11-13 14:43:03 -0700

    Set up file structures

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () =>{
+    displayPets(pets)
+    
+})
+
+function displayPets (pets){
+    const petsContainer = document.querySelector("#content")
+}
