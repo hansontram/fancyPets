110e4bdca7c04f0d62f364af5bfb2fd9a108db01
Hanson Tram <htram.dev@gmail.com>
2023-11-13 15:19:58 -0700

    Restructure javascript file

--- scripts/index.js
+++ scripts/index.js
@@ -5,4 +5,31 @@ document.addEventListener("DOMContentLoaded", () =>{
 
 function displayPets (pets){
     const petsContainer = document.querySelector("#content")
+
+    // clear all elements
+    petsContainer.innerHTML = "";
+
+    pets.forEach(pet => {
+        displayPet(pet, petsContainer)
+    })
+}
+
+
+function displayPet(pet, parentDiv){
+    // create a div for each product
+    const petDiv = document.createElement("div")
+    petDiv.classList.add("pet");
+    petDiv.id = pet.name
+    // add product to the container
+    parentDiv.appendChild(petDiv)
+    console.log(parentDiv.appendChild(petDiv))
+
+}
+function addPetName (pet, petDiv){
+    // create the pet info div
+    const petNameDiv = document.createElement("div")
+    petDiv.appendChild(productHeader)
+    // add pet name
+    const petName = document.createElement("h4")
+    petName.innerText = pet.name
 }
